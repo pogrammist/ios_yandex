0fadcd937b61d6018376b59ec295c4690d0e214f
Rinat Sagitov <sagitovrf@gmail.com>
2020-07-02 17:24:24 +0500

    Fix UIView Nib/Xib part2 033 Example

--- Views/Views/GameControlViewClass.swift
+++ Views/Views/GameControlViewClass.swift
@@ -9,9 +9,74 @@
 import UIKit
 
 @IBDesignable class GameControlViewClass: UIView {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    var startStopHandler: (() -> Void)?
+    
+    @IBInspectable var gameTimeLeft: Double = 7 {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    @IBInspectable var isGameActive: Bool = false {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    @IBInspectable var gameDuration: Double {
+        get {
+            return stepper.value
+        }
+        set {
+            stepper.value = newValue
+            updateUI()
+        }
+    }
+            
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupViews()
+    }
+    
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        updateUI()
+    }
+    
+    @IBAction func actionButtonTapped(_ sender: UIButton) {
+        startStopHandler?()
+    }
+    
+    private func setupViews() {
+        let xibView = loadViewFromXib()
+        xibView.frame = self.bounds
+        xibView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.addSubview(xibView)
+    }
+    
     private func loadViewFromXib() -> UIView {
         let bundle = Bundle(for: type(of: self))
         let nib = UINib(nibName: "GameControlView", bundle: bundle)
         return nib.instantiate(withOwner: self, options: nil).first! as! UIView
+    }
+    
+    private func updateUI() {
+        stepper.isEnabled = !isGameActive
+        if isGameActive {
+            timeLabel.text = "Осталось \(Int(gameTimeLeft)) сек"
+            actionButton.setTitle("Остановить", for: .normal)
+        } else {
+            timeLabel.text = "Время \(Int(stepper.value)) сек"
+            actionButton.setTitle("Начать", for: .normal)
+        }
     }
 }
