4e4eee8e5c325dd719c464a81ffb04bc6d2acaa8
Rinat Sagitov <sagitovrf@gmail.com>
2020-07-04 17:45:13 +0500

    Fix ViewController_demo_table_view Cell in Xib 056 example

---
+++ ViewControllers_demo_table_view/ViewControllers_demo_table_view/RecipeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeTableViewCell.swift
+//  ViewControllers_demo_table_view
+//
+//  Created by user on 04.07.2020.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
